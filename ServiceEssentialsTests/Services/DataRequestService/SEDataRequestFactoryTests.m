3bb34664fc444510fce74a251e64c871c4eade6a
Anton Vaneev <avaneyev@fb.com>
2016-10-15 16:27:09 -0700

    Added a batch of simplistic tests with few parameters.

--- ServiceEssentialsTests/Services/DataRequestService/SEDataRequestFactoryTests.m
+++ ServiceEssentialsTests/Services/DataRequestService/SEDataRequestFactoryTests.m
@@ -13,6 +13,12 @@
 #import "SEDataRequestFactory.h"
 #import "SEInternalDataRequestBuilder.h"
 
+static NSString *const MethodGET = @"GET";
+static NSString *const MethodPOST = @"POST";
+static NSString *const MethodPUT = @"PUT";
+static NSString *const MethodHEAD = @"HEAD";
+
+
 @interface SEDataRequestFactoryTests : XCTestCase
 
 @end
@@ -31,6 +37,7 @@
     [super setUp];
     
     _serviceMock = [OCMockObject mockForProtocol:@protocol(SEDataRequestServicePrivate)];
+    [[[_serviceMock stub] andReturnValue:@(NSUTF8StringEncoding)] stringEncoding];
     _preparationDelegateMock = [OCMockObject mockForProtocol:@protocol(SEDataRequestPreparationDelegate)];
     _userAgentString = @"Mock user agent";
     _baseURL = [NSURL URLWithString:@"https://service.essentials.com"];
@@ -44,9 +51,15 @@
     _preparationDelegateMock = nil;
 }
 
+- (void)veriyAllMocks
+{
+    [_serviceMock verify];
+    [_preparationDelegateMock verify];
+}
+
 - (void)testRequestFactoryThrowsOnUnsafeFactoryCreatingRequestsRequiringSecure
 {
-    SEDataRequestFactory *factory = [[SEDataRequestFactory alloc] initWithService:_serviceMock secure:YES userAgent:_userAgentString requestPreparationDelegate:_preparationDelegateMock];
+    SEDataRequestFactory *factory = [[SEDataRequestFactory alloc] initWithService:_serviceMock secure:NO userAgent:_userAgentString requestPreparationDelegate:nil];
     
     NSError *error = nil;
     XCTAssertThrows([factory createRequestWithMethod:@"GET" baseURL:_baseURL path:@"my_path/method" body:nil mimeType:nil error:&error]);
@@ -57,6 +70,92 @@
     XCTAssertThrows([factory createRequestWithBuilder:builder baseURL:_baseURL error:&error]);
 
     XCTAssertThrows([factory createMultipartRequestWithBuilder:builder baseURL:_baseURL boundary:@"boundary" error:&error]);
+
+    [self veriyAllMocks];
 }
+
+- (void)testRequestFactoryThrowsOnUnsafeFactoryCreatingRequestsRequiringUnsafe
+{
+    SEDataRequestFactory *factory = [[SEDataRequestFactory alloc] initWithService:_serviceMock secure:YES userAgent:_userAgentString requestPreparationDelegate:_preparationDelegateMock];
+
+    NSError *error = nil;
+    XCTAssertThrows([factory createUnsafeRequestWithMethod:@"GET" URL:_baseURL parameters:nil mimeType:nil error:&error]);
+
+    [self veriyAllMocks];
+}
+
+- (void)testRequestFactoryCreateRequestWithoutBodyNoDelegate
+{
+    NSString *const path = @"some-path";
+    SEDataRequestFactory *factory = [[SEDataRequestFactory alloc] initWithService:_serviceMock secure:YES userAgent:_userAgentString requestPreparationDelegate:nil];
+
+    NSError *error = nil;
+    NSURLRequest *request = [factory createRequestWithMethod:MethodGET baseURL:_baseURL path:path body:nil mimeType:nil error:&error];
+
+    XCTAssertNil(error);
+    XCTAssertNotNil(request);
+
+    XCTAssertEqualObjects(request.URL, [NSURL URLWithString:path relativeToURL:_baseURL]);
+    XCTAssertEqualObjects(request.HTTPMethod, MethodGET);
+    XCTAssertNil(request.HTTPBody);
+
+    NSDictionary<NSString *, NSString *> *expectedHeaders = @{
+                                                              @"User-Agent" : _userAgentString,
+                                                              @"Accept" : @"application/json; charset=utf-8"
+                                                              };
+    XCTAssertEqualObjects(request.allHTTPHeaderFields, expectedHeaders);
+
+    [self veriyAllMocks];
+}
+
+- (void)testRequestFactoryCreateGETRequestConvertsBodyToURLQuery
+{
+    NSString *const path = @"other-path";
+    NSDictionary *const parameters = @{ @"first" : @"value", @"second" : @2 };
+    SEDataRequestFactory *factory = [[SEDataRequestFactory alloc] initWithService:_serviceMock secure:YES userAgent:_userAgentString requestPreparationDelegate:nil];
+
+    NSError *error = nil;
+    NSURLRequest *request = [factory createRequestWithMethod:MethodGET baseURL:_baseURL path:path body:parameters mimeType:nil error:&error];
+
+    XCTAssertNil(error);
+    XCTAssertNotNil(request);
+
+    NSURL *expectedURL = [NSURL URLWithString:@"other-path?first=value&second=2" relativeToURL:_baseURL];
+    XCTAssertEqualObjects(request.URL, expectedURL);
+    XCTAssertEqualObjects(request.HTTPMethod, MethodGET);
+    XCTAssertNil(request.HTTPBody);
+
+    NSDictionary<NSString *, NSString *> *expectedHeaders = @{
+                                                              @"User-Agent" : _userAgentString,
+                                                              @"Accept" : @"application/json; charset=utf-8"
+                                                              };
+    XCTAssertEqualObjects(request.allHTTPHeaderFields, expectedHeaders);
+
+    [self veriyAllMocks];
+}
+
+- (void)testRequestFactoryCreateUnsafeRequestWithoutBody
+{
+    NSURL *url = [_baseURL URLByAppendingPathComponent:@"some-other-path"];
+    SEDataRequestFactory *factory = [[SEDataRequestFactory alloc] initWithService:_serviceMock secure:NO userAgent:_userAgentString requestPreparationDelegate:nil];
+
+    NSError *error = nil;
+    NSURLRequest *request = [factory createUnsafeRequestWithMethod:MethodGET URL:url parameters:nil mimeType:nil error:&error];
+
+    XCTAssertNil(error);
+    XCTAssertNotNil(request);
+
+    XCTAssertEqualObjects(request.URL, url);
+    XCTAssertEqualObjects(request.HTTPMethod, MethodGET);
+    XCTAssertNil(request.HTTPBody);
+
+    NSDictionary<NSString *, NSString *> *expectedHeaders = @{
+                                                              @"User-Agent" : _userAgentString,
+                                                              };
+    XCTAssertEqualObjects(request.allHTTPHeaderFields, expectedHeaders);
+
+    [self veriyAllMocks];
+}
+
 
 @end
